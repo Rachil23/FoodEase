b2f75b68c9e8d9525f0d9822db422f30ccf3bec3
Rachil23 <rachilnamdev72@gmail.com>
2023-05-18 15:14:48 +0530

    Cart update

--- app/src/main/java/com/example/foodeaseapp/MainActivity8.java
+++ app/src/main/java/com/example/foodeaseapp/MainActivity8.java
@@ -6,10 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity8 extends AppCompatActivity {
     private Button button;
     private Button b;
+    private Button bu;
+    private Button but;
+    private Button butto;
+    private Button bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +38,17 @@ public class MainActivity8 extends AppCompatActivity {
             }
         }
         );
+        bu = findViewById(R.id.button23);
+        bu.setOnClickListener(this::onClick2);
+
+        but = findViewById(R.id.button24);
+        but.setOnClickListener(this::onClick3);
+
+        butto = findViewById(R.id.button25);
+        butto.setOnClickListener(this::onClick4);
+
+        bt = findViewById(R.id.button26);
+        bt.setOnClickListener(this::onClick5);
     }
 
     public void openActivity344() {
@@ -43,5 +59,21 @@ public class MainActivity8 extends AppCompatActivity {
     public void openActivity444() {
         Intent intent = new Intent(this, MainActivity3.class);
         startActivity(intent);
+    }
+
+    public void onClick2(View view){
+        Toast.makeText(MainActivity8.this,"Coffee added to order list", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClick3(View view){
+        Toast.makeText(MainActivity8.this,"Redbull added to order list", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClick4(View view){
+        Toast.makeText(MainActivity8.this,"Sprite added to order list", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClick5(View view){
+        Toast.makeText(MainActivity8.this,"Sting added to order list", Toast.LENGTH_SHORT).show();
     }
 }
