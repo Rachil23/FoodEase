982e3afddec8ba365fd1cf36bbb90628bddde353
Rachil23 <rachilnamdev72@gmail.com>
2023-05-19 22:33:18 +0530

    Toast error solved and major button updates

--- app/src/main/java/com/example/foodeaseapp/MainActivity4.java
+++ app/src/main/java/com/example/foodeaseapp/MainActivity4.java
@@ -3,6 +3,7 @@ package com.example.foodeaseapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,20 +12,29 @@ import android.widget.Toast;
 
 public class MainActivity4 extends AppCompatActivity implements View.OnClickListener {
     private Button button;
+    private Button b;
 
     EditText id_email;
 
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
         id_email=findViewById(R.id.delivery_address);
         button = findViewById(R.id.button7);
         button.setOnClickListener(this);
+
+        b = (Button) findViewById(R.id.button31);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                                          openActivity644();
+                                      }
+        }
+        );
     }
 
     public void onClick(View view){
@@ -36,4 +46,10 @@ public class MainActivity4 extends AppCompatActivity implements View.OnClickList
         else{
         Toast.makeText(MainActivity4.this,"Order confirmed ", Toast.LENGTH_SHORT).show();}
     }
+    public void openActivity644() {
+        Intent intent = new Intent(this, MainActivity3.class);
+        startActivity(intent);
+    }
+
+
 }
