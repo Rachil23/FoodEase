5b8f12e3bdd278f0e2597c480e9691f4ba6bce01
Saket <saket.bagdi@gmail.com>
2023-05-19 11:58:19 +0530

    Cart update

--- app/src/main/java/com/example/foodeaseapp/Cart.java
+++ app/src/main/java/com/example/foodeaseapp/Cart.java
@@ -1,10 +1,11 @@
 package com.example.foodeaseapp;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 public class Cart {
-
     private String foodName;
     private String foodPrice;
     private String foodPhoto;
